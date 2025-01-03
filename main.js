c5d952f89e0839f080e3a99c56a6a29fb0123f64
percoholic99 <slypacan4ik@gmail.com>
2025-01-03 23:52:51 +0300

    добавил файлы в dinarJS

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
-console.log('1' + 5);
-console.log(1 + 'krasava');
-console.log(false + false);
-console.log(true + true);
-console.log(Boolean(1) + String(" guys studiyng in litsey ") + Number(79));
-console.log(typeof 79 + " " + typeof "litsey" + " " + typeof Boolean)
-const a = " Litsey "
-const b = Number(79)
-console.log(a+b)
-console.log(" litsey " + Number(79))
-console.log
-console.log
-console.log
-console.log
-console.log
-console.log
+function calc(operation, a, b) {
+switch (operation) {
+case 'add':
+ return a + b;
+ case 'multi' :
+ return a * b;
+ case 'subtract':
+    return a - b;
+}
+}
+
+console.log(calc('add', 1, 2));      
+console.log(calc('multi', 1, 2));    
+console.log(calc('subtract', 3, 2));
