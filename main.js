891dd518f43fa99c2a68ffe062ab7f504a4f9a26
percoholic99 <slypacan4ik@gmail.com>
2025-01-04 01:39:45 +0300

    добавил main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+function calc(operation, a, b) {
+switch (operation) {
+case 'add':
+ return a + b;
+ case 'multi' :
+ return a * b;
+ case 'subtract':
+    return a - b;
+}
+}
+
+console.log(calc('add', 1, 2));      
+console.log(calc('multi', 1, 2));    
+console.log(calc('subtract', 3, 2));
