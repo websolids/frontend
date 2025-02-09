f0a744488aafb09d13bc6629f6770b7ecc787667
percoholic99 <slypacan4ik@gmail.com>
2025-02-09 16:32:13 +0300

    день 26

---
+++ dinarJS/main.js
@@ -0,0 +1,10 @@
+const input = document.getElementById("userInput")
+console.log(input)
+const button = document.getElementById("myButton")
+console.log(button)
+
+const inp = document.querySelector("input")
+const btn = document.querySelector("button")
+
+console.log(inp)
+console.log(btn)
